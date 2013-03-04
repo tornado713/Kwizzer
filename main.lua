d0b8ebead6bff2c645461266ec906b25f260375a
Michael Kidd <tornado713@gmail.com>
2013-03-04 16:23:11 -0700

    added first scene

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
+
+local storyboard = require "storyboard"
+local widget = require "widget"
 -----------------------------------------------------------------------------------------
 --
 -- main.lua
@@ -5,3 +8,42 @@
 -----------------------------------------------------------------------------------------
 --Debug the smart way with Cider!
 --start coding and press the debug button
+
+local db = require("db")
+
+db:initialize{}
+db:printStuff{}
+
+local function onSystemEvent( event )
+        if( event.type == "applicationExit" ) then              
+            db:close()
+        end
+end
+
+storyboard.gotoScene( "menu" )
+
+--local loadBtn = widget.newButton{
+--    label = "Load",
+--    labelColor = { default={0}, over={0} },
+--    font = native.systemFontBold,
+--    xOffset=2, yOffset=-1,
+----    default = "load-default.png",
+----    over = "load-over.png",
+--    width=50, height=25,
+--    left=10, top=28
+--}
+--
+---- onRelease listener callback for loadBtn
+--local function onLoadRelease( event )
+--    titleField.text = "woot"
+--    print(titleField.text)
+--end
+--loadBtn.onRelease = onLoadRelease
+--
+--local textFont = native.newFont( native.systemFont )
+--
+--titleField = native.newTextBox(10, 90, 100, 30)
+--titleField.font = textFont
+--titleField.text = "test"
+--titleField.size = 14
+--    print(titleField.text)
